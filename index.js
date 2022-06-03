3b916f22754835d1e9cf77abd088577bad4a0bcf
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-06-03 02:28:29 +0100

    Dynamically render pages based on url path

--- index.js
+++ index.js
@@ -3,11 +3,30 @@ const fs = require('fs');
 
 const port = process.env.PORT || 8080;
 
+const urls = [
+  '/',
+  '/contact-me',
+  '/about'
+];
+
+const pages = [
+  './index.html',
+  './contact-me.html',
+  './about.html'
+]
+
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  html = fs.readFileSync('./index.html');
-  res.end(html);
+  if (urls.indexOf(req.url) > -1) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    html = fs.readFileSync(pages[urls.indexOf(req.url)]);
+    res.end(html);
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    html = fs.readFileSync('./404.html');
+    res.end(html);
+  }
 });
 
 server.listen(port, () => {
