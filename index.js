76fc6427be4453b5e82d15003698c69eb5458671
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-06-04 22:46:57 +0100

    Add comments

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+/**
+ * Require Modules
+ */
 const http = require('http');
 const fs = require('fs');
 
+/**
+ * Define Relevant Variables
+ */
 const port = process.env.PORT || 8080;
 
 const urls = [
@@ -13,7 +19,11 @@ const pages = [
   './index.html',
   './contact-me.html',
   './about.html'
-]
+];
+
+/**
+ * Create Server
+ */
 
 const server = http.createServer((req, res) => {
   if (urls.indexOf(req.url) > -1) {
@@ -28,6 +38,10 @@ const server = http.createServer((req, res) => {
     res.end(html);
   }
 });
+
+/**
+ * Listen on assigned port
+ */
 
 server.listen(port, () => {
   console.log(`Running at port ${port}`);
