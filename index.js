506c64300327acb56eb125ddc756fe7a8aa4035f
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-06-06 14:01:22 +0100

    Apply standard.js linting

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 /**
  * Require Modules
  */
-const http = require('http');
-const fs = require('fs');
+const http = require('http')
+const fs = require('fs')
 
 /**
  * Define Relevant Variables
  */
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 
 const urls = [
   '/',
   '/contact-me',
   '/about'
-];
+]
 
 const pages = [
   './index.html',
   './contact-me.html',
   './about.html'
-];
+]
 
 /**
  * Create Server
@@ -27,22 +27,22 @@ const pages = [
 
 const server = http.createServer((req, res) => {
   if (urls.indexOf(req.url) > -1) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    html = fs.readFileSync(pages[urls.indexOf(req.url)]);
-    res.end(html);
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    const html = fs.readFileSync(pages[urls.indexOf(req.url)])
+    res.end(html)
   } else {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    html = fs.readFileSync('./404.html');
-    res.end(html);
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    const html = fs.readFileSync('./404.html')
+    res.end(html)
   }
-});
+})
 
 /**
  * Listen on assigned port
  */
 
 server.listen(port, () => {
-  console.log(`Running at port ${port}`);
-});
+  console.log(`Running at port ${port}`)
+})
